8b84a211e3efebfb2c86b2e1201fa1243607bd43
IllgiLP <cornelius3@hotmail.com>
2019-01-13 20:29:35 +0100

    autostart added

---
+++ w3dprinter-client/boot/w3dprinter.lua
@@ -0,0 +1,27 @@
+local vcomp = require("vcomponent")
+local ser = require("serialization")
+local fs = require("filesystem")
+local shell = require("shell")
+
+local function readConfig(file)
+  local done,fil = pcall(fs.open,file,"r");
+  if not (done) then
+    shell.execute("wget -f https://raw.githubusercontent.com/IllgiLP/OpenComputers-Programs/master/w3dprinter-client/etc/w3dprinter.cfg /etc/w3dprinter.cfg")
+    return readConfig(file);
+  end
+  local content = fil:read("*a")
+  local don,tbl = pcall(ser.unserialize,content)
+  if not (don) then
+    shell.execute("wget -f https://raw.githubusercontent.com/IllgiLP/OpenComputers-Programs/master/w3dprinter-client/etc/w3dprinter.cfg /etc/w3dprinter.cfg")
+    return readConfig(file);
+  end
+  return tbl;
+end
+
+--[[local proxy = {
+	test = function(something) return type(something) end
+}
+local docs = {
+	test = "function(value:something):string -- I do stuff."
+}
+vcomp.register("LALWZADDR","testcomp",proxy,docs)]]--
