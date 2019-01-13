5d5a7bf499795249c87970e3e01b330bd031dd94
IllgiLP <cornelius3@hotmail.com>
2019-01-13 18:35:45 +0100

    added server

---
+++ w3dprinter-server/w3dprinter.lua
@@ -0,0 +1,10 @@
+local shell = require("shell")
+
+local args, options = shell.parse(...)
+
+local function printUsage()
+  print("Usage:")
+  print("'w3dprinter' to show this message")
+end
+
+printUsage()
