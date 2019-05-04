9ad3cfc7500fae4edfabda2f8e267186564905b6
IllgiLP <cornelius3@hotmail.com>
2019-05-04 15:59:17 +0200

    update

--- shop/etc/rc.d/shop.lua
+++ shop/etc/rc.d/shop.lua
@@ -3,13 +3,6 @@ local event = require("event")
 local shop = loadfile("/program_files/shop/shop.lua")()
 
 function start()
-  if(timerId ~= nil) then
-    print("Already started!")
-  else
-    timerId = event.timer(0.5,function()
-      print("HI")
-    end,math.huge)
-  end
   if(shop.isRunning()) then
     print("Already started!")
   else
@@ -18,12 +11,6 @@ function start()
 end
 
 function stop()
-  if(timerId == nil) then
-    print("Not running!")
-  else
-    event.cancel(timerId)
-    timerId = nil
-  end
   if not (shop.isRunning()) then
     print("Not running!")
   else
